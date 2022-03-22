bdfff307118dc72855b403a2c38068122b55d1c2
Aaron Regalado <regalado2829@gmail.com>
2022-03-22 09:03:52 -0400

    Finished the Script for 'Rock, Paper, Scissors'
    I finished the script for the game. I put the function that runs the
    game in a 'for' loop so that it can run 5 times. The script also
    prints who won and how many times they won.

--- run.js
+++ run.js
@@ -1,33 +1,41 @@
+let player_wins = 0;
+let computer_wins = 0;
 function computerPlay(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (computerSelection === 0) {
         if (playerSelection === "rock") {
-            console.log("Tie! Rock can't beat rock!")
+            console.log("Tie! Rock can't beat rock!");
         }
         else if (playerSelection === "paper") {
             console.log("You win! Paper beats rock!")
+            player_wins = player_wins + 1
         }
         else if (playerSelection === "scissors") {
             console.log("You lose! Rock beats scissors!")
+            computer_wins = computer_wins + 1
         }
     }
     else if (computerSelection === 1) {
         if (playerSelection === "rock") {
             console.log("You lose! Paper beats Rock!")
+            computer_wins = computer_wins + 1
         }
         else if (playerSelection === "paper") {
             console.log("Tie! Paper can't beat paper!")
         }
         else if (playerSelection === "scissors") {
             console.log("You win! Scissors beat paper!")
+            player_wins = player_wins + 1
         }
     }
     else if (computerSelection === 2) {
         if (playerSelection === "rock") {
             console.log("You win! Rock beats scissors!")
+            player_wins = player_wins + 1
         }
         else if (playerSelection === "paper") {
             console.log("You lose! Scissors beat paper.")
+            computer_wins = computer_wins + 1
         }
         else if (playerSelection === "scissors") {
             console.log("Tie! Scissors can't beat scissors!")
@@ -36,8 +44,24 @@ function computerPlay(playerSelection, computerSelection) {
 }
 
 
-computerPlay(prompt("Type rock, paper, or scissors: "), Math.floor(Math.random() * (["Rock", "Paper", "Scissors"].length)));
-/* The computer picks a random number. This number has to be rounded down in order to be
-an integer. Since Math.random returns a number between 0 and 1 on its own, 
-I took the length of the list and multiplied it by that number. This makes Math.random return a
-number between 0 and 3. */ 
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Type rock, paper, or scissors: ");
+    let computerSelection = Math.floor(Math.random() * (["Rock", "Paper", "Scissors"].length));
+    computerPlay(playerSelection, computerSelection);
+    /* The computer picks a random number. This number has to be rounded down in order to be
+    an integer. Since Math.random returns a number between 0 and 1 on its own, 
+    I took the length of the list and multiplied it by that number. This makes Math.random return a
+    number between 0 and 3. */ 
+
+ }
+
+ console.log("You won " + player_wins + " times. The computer won " + computer_wins + " times.")
+ if (computer_wins > player_wins) {
+    console.log("The Computer wins!")
+ }
+ else if (player_wins > computer_wins) {
+    console.log("You win!")
+ }
+ else if (player_wins = computer_wins) {
+    console.log("Tie!")
+ }
